9ab0e7c5d5819bca6d0bcb007e91f1dc435a2186
nhgggggggh <42756203+nhgggggggh@users.noreply.github.com>
2025-01-16 15:15:33 -0500

    Update gay.py

--- gay.py
+++ gay.py
@@ -1,1 +1,10 @@
-print("gfd")
+gay = False
+
+for i in range(10):
+    if (i == 10):
+        gay = True
+    if (gay == True):
+        print("Oh you gay af now...")
+    else:
+        print("Nah you chill af")
+
